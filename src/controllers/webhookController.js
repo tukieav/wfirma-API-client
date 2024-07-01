88d792f27eafddbece36f552f180582767bcc883
bartek <bartek.lazarowicz@gmail.com>
2024-07-01 22:33:30 +0200

    new webhookController

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -3,7 +3,7 @@ exports.handleWebhook = (req, res) => {
     console.log('Webhook received:', data);
 
     // Przykładowy klucz webhooka, który powinien być unikalny dla każdej domeny
-    const webhookKey = '50f1811d67487937bdd53dae49043e56';
+    const webhookKey = 'aca700257b337ba79e6254232d60ec99';
 
     // Przetwarzanie danych z webhooka
     res.status(200).json({ webhook_key: webhookKey });
