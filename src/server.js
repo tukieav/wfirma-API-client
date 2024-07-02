01c223e2312d19647716ef7b66394e0eb4229ab5
bartek <bartek.lazarowicz@gmail.com>
2024-07-02 09:32:17 +0200

    zmiany w server.js zwiazane z wyszukiwaniem webhookow

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const config = require('./config/config');
 const axios = require('axios');
+const wfirmaService = require('./services/wfirmaService');
 
 // Funkcja do sprawdzenia autoryzacji do API wFirma
 const checkWfirmaAuth = async () => {
@@ -26,14 +27,33 @@ const getPublicIp = async () => {
         const { publicIpv4 } = await import('public-ip');
         const ip = await publicIpv4();
         console.log(`Server public IP: ${ip}`);
+        return ip;
     } catch (error) {
         console.error('Error fetching public IP:', error.message);
     }
 };
 
+// Funkcja do zarządzania webhookami
+const manageWebhooks = async (publicIp) => {
+    try {
+        const webhooks = await wfirmaService.getWebhooks();
+        const existingWebhook = webhooks.find(wh => wh.url === `http://${publicIp}:3000/webhook`);
+
+        if (existingWebhook) {
+            console.log('Webhook already exists:', existingWebhook);
+        } else {
+            const newWebhook = await wfirmaService.createWebhook(`http://${publicIp}:3000/webhook`);
+            console.log('Created new webhook:', newWebhook);
+        }
+    } catch (error) {
+        console.error('Error managing webhooks:', error.message);
+    }
+};
+
 const startServer = async () => {
     await checkWfirmaAuth();
-    await getPublicIp();
+    const publicIp = await getPublicIp();
+    await manageWebhooks(publicIp);
 
     const PORT = config.port || 3000;
     app.listen(PORT, () => {
