ab1d56de4c9c81dc4cf11555963d9cf73a3e4be1
bartek <bartek.lazarowicz@gmail.com>
2024-07-03 15:42:48 +0200

    fc

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -1,17 +1,17 @@
 const config = require('../config/config');
-const objectParse = require('../utils/objectParser');
+const { parse } = require('../utils/objectParser');
 
 exports.handleWebhook = (req, res) => {
     const data = req.body;
     console.log('Webhook received:', data);
 
     const { invoices, status, metainfo } = data;
-    const invoiceDetails = invoices['0'].invoice;
+    const invoiceDetails = parse(invoices['0'].invoice);
     const { limit, page, total } = invoices.parameters;
     const { code } = status;
     const { company_id, created, object_id, object_name, webhook_event, webhook_id } = metainfo;
 
-    console.log('Invoice Details:', objectParse(invoiceDetails));
+    console.log('Invoice Details:', invoiceDetails);
     console.log('Parameters:', { limit, page, total });
     console.log('Status Code:', code);
     console.log('Metainfo:', { company_id, created, object_id, object_name, webhook_event, webhook_id });
