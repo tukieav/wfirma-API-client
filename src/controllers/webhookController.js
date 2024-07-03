bacd798c72117abd005863b9d503bf79d3bb8379
bartek <bartek.lazarowicz@gmail.com>
2024-07-03 15:37:54 +0200

    fc

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -6,12 +6,12 @@ exports.handleWebhook = (req, res) => {
     console.log('Webhook received:', data);
 
     const { invoices, status, metainfo } = data;
-    const invoiceDetails = objectParse(invoices['0'].invoice);
+    const invoiceDetails = invoices['0'].invoice;
     const { limit, page, total } = invoices.parameters;
     const { code } = status;
     const { company_id, created, object_id, object_name, webhook_event, webhook_id } = metainfo;
 
-    console.log('Invoice Details:', invoiceDetails);
+    console.log('Invoice Details:', objectParse(invoiceDetails));
     console.log('Parameters:', { limit, page, total });
     console.log('Status Code:', code);
     console.log('Metainfo:', { company_id, created, object_id, object_name, webhook_event, webhook_id });
